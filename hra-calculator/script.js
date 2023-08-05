038563050342cc79aa8b715962360c380cf6854e
Thirupathi Thangavel <thirupathi.thangavel@gmail.com>
2023-08-05 22:05:45 +0530

    fix HRA metro city

--- hra-calculator/script.js
+++ hra-calculator/script.js
@@ -5,7 +5,7 @@ function calculateHRA() {
     const metro_city = document.getElementById("metro_city").checked;
 
     const basic_ratio = metro_city ? 0.50 : 0.40;
-    const basic_40 = 0.40 * basic_pay;
+    const basic_40 = basic_ratio * basic_pay;
     let rent_over_basic_10 = rent - 0.10 * basic_pay;
     rent_over_basic_10 = Math.max(0, rent_over_basic_10);
     const result = Math.min(basic_40, hra, rent_over_basic_10);
